80187b6990cbd74a703cd1b5aa65aa56d340ac22
Antoine Grand <antoine.grand@epitech.eu>
2023-11-26 13:24:38 +0100

    Add the last days. Remains to do the last day. And add the month verification.

--- scripts/daysGift.js
+++ scripts/daysGift.js
@@ -30,24 +30,26 @@ let DaysGifts = [
         content: "<p>Bravo à toi mon amour, je te l'accorde c'était simple ! Mais bravo pour ça !</p><p>En récompense je t'offre ce petit pendentif avec le symbole de l'amour</p><img src='./ressources/img/pendentif.jpeg'><h2>❤</h2>"
     },
     {
-        type: 'question',
-        questionContent: "<h1>Question plus difficile aujourd'hui !</h1><p>Qu'est-ce qui s'est passé le 29 octobre 2023 ?</p><p>Indice : ça faisait assez longtemps qu'on attendait ça !</p>",
-        choices: ['gagnant', 'tok', 'toque'],
-        content: "<h1>Bravo pour celle là elle était pas simple !</h1><p>Mais on peut pas rester sur une victoire...</p><img src='./ressources/img/TokWinner.jpeg'><h2>Bon pour remettre la paté à mes parents au tok ! ❤</h2>"
-    },
-    {
         type: 'text',
         content: "<p>Petit jour assez simple, prends ça en photo et tu le sors quand tu veux !</p><img src='./ressources/img/dej.jpg'><h2>Bon pour un ptit dej au lit ❤</h2>"
     },
     {
-        type: 'text',
-        content: "<p>photo de cet été, essayer de mettre les 3 en carroussel</p>"
+        type: 'question',
+        questionContent: "<h1>Question plus difficile aujourd'hui !</h1><p>Qu'est-ce qui s'est passé le 29 octobre 2023 ?</p><p>Indice : ça faisait assez longtemps qu'on attendait ça !</p>",
+        choices: ['gagnant', 'tok', 'toque'],
+        content: "<h1>Bravo pour celle là, elle était pas simple !</h1><p>Mais on peut pas rester sur une victoire...</p><img src='./ressources/img/TokWinner.jpeg'><h2>Bon pour remettre la paté à mes parents au tok ! ❤</h2>"
     },
     {
         type: 'question',
-        questionContent: "<p>Quel est un plat que j'ai kiffé faire avec toi ?</p><p>Indice : tu as fait un BeReal où je te trouve juste sublime où on a cuisiné ce plat.</p>",
+        questionContent: "<h1>Question souvenir</h1><img src='./ressources/img/kruthCave.jpeg'><p>Où a été prise cette photo ?</p>",
+        choices: ['lac', 'kruth'],
+        content: "<h1>Bravo à toi mon ange !</h1><img src='./ressources/img/summer.jpeg'><h2>J'en garde tellement un beau souvenir de cet été et de nos petites balades un peu partout ❤</h2>"
+    },
+    {
+        type: 'question',
+        questionContent: "<h2>Quel est un plat que j'ai kiffé faire avec toi ?</h2><p>Indice : tu as fait un BeReal où je te trouve juste sublime où on a cuisiné ce plat.</p>",
         choices: ['sushi', 'sushis'],
-        content: "<p>Bravo à toi mon ange !</p><p>Le cadeau d'aujourd'hui tu connais la chanson, prends en photo et sors moi ça quand tu veux.</p><img src='./ressources/img/beReal.png'><h2>Bon pour qu'on se cuisine des sushis en amoureux ❤</h2>"
+        content: "<h1>Bravo à toi mon ange !</h1><p>Le cadeau d'aujourd'hui tu connais la chanson, prends en photo et sors moi ça quand tu veux.</p><img src='./ressources/img/beReal.png'><h2>Bon pour qu'on se cuisine des sushis en amoureux ❤</h2>"
     },
     {
         type: 'text',
@@ -60,24 +62,26 @@ let DaysGifts = [
         content: "<p>Bravo à toi mon chat !</p><p>Le cadeau d'aujourd'hui tu le prends en photo et tu me sors cette photo dès que tu le sens.</p><img src='./ressources/img/massage.jpg'><h2>Bon pour que je pose mes mains sur ton corps pour un massage ❤</h2>"
     },
     {
-        type: 'text',
-        content: "<p>jour à trouver</p>"
+        type: 'question',
+        questionContent: "<h1>Question souvenir</h1><img src='./ressources/img/Cascade.jpeg'><p>Où a été prise cette magnifique photo ?</p>",
+        choices: ['lac', 'seven'],
+        content: "<p>C'était trop bien là bas !</p><img src='./ressources/img/Seven.jpeg'><h2>Comme à Kruth, notre petite balade et notre baignade était vraiment trop bien ! ❤</h2>"
     },
     {
         type: 'text',
-        content: "<p>Encore un petit jour simple où je te fais gagner des bons moment avec moi !</p><p>Cette fois on change un peu tu peux le faire un peu avant qu'on se retrouve !</p><img src='./ressources/img/cookies.jpg'><h2>Bon pour nous faire une petite séance de cuisine cookies à distance ❤</h2>"
+        content: "<p>Encore un petit jour simple où je te fais gagner des bons moment avec moi !</p><p>Cette fois on change un peu, tu peux l'utiliser un peu avant qu'on se retrouve !</p><img src='./ressources/img/cookies.jpg'><h2>Bon pour nous faire une petite séance de cuisine cookies à distance ❤</h2>"
     },
     {
         type: 'text',
-        content: "<h1>Indice pour un de tes cadeaux de Noël</h1><p>Pour celle là je vais pas te donner trop d'indices :</p><p>Tout ce que je peux te dire c'est que quand tu fais à manger et que tu regardes Friends, maintenant tu vas voir double !</p><img src='./ressources/img/friends.webp'>"
+        content: "<h1>Indice pour un de tes cadeaux de Noël</h1><p>Pour celui là je vais pas te donner trop d'indices :</p><p>Tout ce que je peux te dire c'est que quand tu fais à manger et que tu regardes Friends, maintenant tu vas voir double !</p><img src='./ressources/img/friends.webp'>"
     },
     {
         type: 'text',
-        content: "<p>poème</p>"
+        content: "<p>Comme promis, je participe à ta robe de Nouvel An et aujourd'hui c'est un jour pour moi !</p><p>Au moment où j'écris ça je ne sais pas ce qu'a répondu la fille de Vinted !</p><h2>En tout cas trois possibilités :</h2><p>Soit elle t'as répondu et tu ne l'as pas encore pris, à ce moment là je te demande de la prendre et je te payerai la moitié !</p><p>Soit elle t'as répondu et tu ne l'as pas pris, auquel cas je veux bien qu'on en choisisse une ensemble si tu veux !</p><p>Ou alors dernière possibilité, tu l'as reçu et à ce moment là je veux une belle photo de toi dans cette robe ! ❤</p>"
     },
     {
         type: 'text',
-        content: "<p>indice parfum</p>"
+        content: "<h1>Indice pour un autre de tes cadeaux de Noël</h1><p>Pour ce cadeau de Noël je vais faire exprès de pas trop te donner de pistes pour que tu aies quand même la surprise !</p><p>Tout ce que je peux dire c'est que pour une fois je vais changer mes goûts :<p><h2>Pour une fois, je vais kiffer le café ! ❤</h2>"
     },
     {
         type: 'question',
